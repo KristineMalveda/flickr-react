d48d4bb439d14543e426c38cbffb542b86ccf1f4
Kristinemalveda1985 <kristinemalveda@yahoo.com>
2020-09-17 23:26:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Search from "./Search";
 
-function App() {
-  return (
-    <div className="App">
-        Hello World
-    </div>
-  );
+class App extends Component {
+  state = {
+    pictures: [],
+  };
+
+  componentDidMount = () => {
+    this.search();
+  };
+
+  search = (inputvalue) => {
+    fetch(
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=81e61d57864118cacd51f259fcc3c173&tags=${inputvalue}&per_page=30&page=1&format=json&nojsoncallback=1`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        let picArray = json.photos.photo.map((pic) => {
+          var farmId = pic.farm;
+          var serverId = pic.server;
+          var id = pic.id;
+          var secret = pic.secret;
+         
+          var srcPath =
+            "https://farm" +
+            farmId +
+            ".staticflickr.com/" +
+            serverId +
+            "/" +
+            id +
+            "_" +
+            secret +
+            ".jpg";
+            
+          return (
+            <li key = {id}>
+              <img  alt="pix" src={srcPath}></img>
+            </li>
+          );
+        });
+        this.setState({ pictures: picArray});
+        
+      })
+      .catch((error) => {
+        console.log("Oops! Something went wrong...");
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Search searchValue={this.search} />
+          <div className="searchresults">
+            <ul className="searchresults__list">{this.state.pictures}</ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Search.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Search extends Component {
+  state = {inputvalue: ""};
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchValue(this.state.inputvalue);
+    this.setState({inputvalue:""});
+  };
+  onChange = (e) => {
+    this.setState({inputvalue: e.target.value});
+  };
+
+  render() {
+    return (
+      <form className="controls" onSubmit={this.onSubmit}>
+        <div className="searchField">
+          <input
+            type="text"
+            placeholder="Search Flickr for Photos.. ."
+            name="title"
+            value={this.state.inputvalue}
+            style={{
+              border: "1px solid grey",
+              borderRadius: "4px",
+              height: "40px",
+              width: "400px",
+              fontSize: "20px",
+              padding: "10px",
+              marginRight: "10px",
+            }}
+            onChange={this.onChange}
+          ></input>
+
+          <input
+            name="submit"
+            type="submit"
+            value="Submit"
+            style={btnStyle}
+          ></input>
+        </div>
+        <div className="gallery-button__wrapper">
+          <input
+            style={btnStyle}
+            className="gallery"
+            type="button"
+            value="Show Gallery"
+          ></input>
+        </div>
+      </form>
+    );
+  }
+}
+
+const btnStyle = {
+  minWidth: "60px",
+  height: "40px",
+  fontSize: "14px",
+  lineHeight: "24px",
+  padding: "3px",
+  borderRadius: "10px",
+  border: "2px solid #616161",
+  backgroundColor: "white",
+};
+export default Search;
+//proptypes
+Search.propTypes = {
+  searchValue: PropTypes.func.isRequired,
+};
+
